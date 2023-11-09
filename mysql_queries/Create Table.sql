749b15cec3cd5c54101ddd981f8110c0d77bd490
ziongeek <josephomidiora@gmail.com>
2023-11-09 23:23:12 +0100

    Day 2 - EDA on Employee dataset

---
+++ mysql_queries/Create Table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Employees (
+    education VARCHAR(255),
+    joining_year VARCHAR(4), -- Assuming the joining year is a 4-digit value
+    city VARCHAR(255),
+    payment_tier INT,
+    age INT,
+    gender VARCHAR(10), -- Assuming gender is a short string
+    everBenched BOOLEAN,
+    experience_in_domain INT,
+    leave_or_not BOOLEAN
+);
