f796a77a3a8204401e55573c70f956c0e5870225
ziongeek <josephomidiora@gmail.com>
2023-11-19 19:33:18 +0100

    Google DA Hands on practise

--- mysql_queries/Create Table.sql
+++ mysql_queries/Create Table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Employees (
+CREATE TABLE IF NOT EXISTS Employees (
     education VARCHAR(255),
     joining_year VARCHAR(4), -- Assuming the joining year is a 4-digit value
     city VARCHAR(255),
@@ -9,3 +9,27 @@ CREATE TABLE Employees (
     experience_in_domain INT,
     leave_or_not BOOLEAN
 );
+
+CREATE TABLE IF NOT EXISTS car (
+    make VARCHAR(255),
+    fuel_type VARCHAR(255),
+    num_of_doors INT, -- Assuming num_of_doors is an integer
+    body_style VARCHAR(255),
+    drive_wheels VARCHAR(255),
+    engine_location VARCHAR(255),
+    wheel_base FLOAT,
+    length FLOAT,
+    width FLOAT,
+    height FLOAT,
+    curb_weight INT,
+    engine_type VARCHAR(255),
+    num_of_cylinders VARCHAR(255),
+    engine_size INT,
+    fuel_system VARCHAR(255),
+    compression_ratio INT,
+    horsepower INT,
+    city_mpg INT,
+    highway_mpg INT,
+    price INT
+);
+
